2a34954d7d8f29d228dfb6fa21e03541fd1f9193
ToryangChen <ToryangChen@outlook.com>
2015-05-22 12:56:51 +0800

    new Test

--- src/com/Factory/HNBoy.java
+++ src/com/Factory/HNBoy.java
@@ -4,6 +4,7 @@ package com.Factory;
  * Created by Toryang on 2015/4/18.
  */
 public class HNBoy implements Boy {
+    //new Test!
     @Override
     public void DrawBoy() {
         System.out.println("new year boy");
